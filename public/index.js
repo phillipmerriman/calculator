9c11bfb233e03aed832f6ddd139065810a08fe02
Phillip Merriman <phillipmerriman@gmail.com>
2021-02-17 17:16:56 -0600

    removed location reload. wasnt needed

--- public/index.js
+++ public/index.js
@@ -106,7 +106,6 @@ $(document).ready(function () {
       },
     }).then(function (data) {
       $("#results-card").empty();
-      location.reload();
       initializeCalculator();
     });
   });
