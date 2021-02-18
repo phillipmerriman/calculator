e5f89fc9edea6292988d40090fcb9c6b727433ca
Phillip Merriman <phillipmerriman@gmail.com>
2021-02-18 12:13:00 -0600

    changed socket io connection

--- public/index.js
+++ public/index.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
   //========================================
   // Make websocket connection
   
-  let socket = io.connect( process.env.PORT ||"http://localhost:3000");
+  let socket = io.connect("https://desolate-ocean-97835.herokuapp.com/");
   //========================================
 
   // Use a function to initialize our calculator.
